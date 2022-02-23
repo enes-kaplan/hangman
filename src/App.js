ff7639341e9a0f10b91a5f24d6d756191768f5c5
Enes Kaplan <eneskaplan94@hotmail.com>
2022-02-23 16:28:40 +0300

    Added state params

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { useState } from 'react'
 import './App.css';
 import Header from './components/Header'
 import Figure from './components/Figure'
 import WrongLetters from './components/WrongLetters'
 import Word from './components/Word'
 
+const words = ['application', 'programming', 'interface', 'wizard'];
+
 function App() {
+	const [playable, setPlayable] = useState(true)
+	const [correctLetters, setCorrectLetters] = useState([])
+	const [wrongLetters, setWrongLetters] = useState([])
+
 	return (
 		<>
 			<Header />

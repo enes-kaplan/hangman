af59d6a1dae44ecc455399022f37595f818c49d0
Enes Kaplan <eneskaplan94@hotmail.com>
2022-02-24 08:51:00 +0300

    Added keyboard listener for typing the word

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css';
 import Header from './components/Header'
 import Figure from './components/Figure'
@@ -13,6 +13,33 @@ function App() {
 	const [correctLetters, setCorrectLetters] = useState([])
 	const [wrongLetters, setWrongLetters] = useState([])
 
+	useEffect(() => {
+		const handleKeydown = event => {
+			const { key, keyCode } = event
+			if (playable && keyCode >= 65 && keyCode <= 90) {
+				const letter = key.toLowerCase()
+
+				if (selectedWord.includes(letter)) {
+					if (!correctLetters.includes(letter)) {
+						setCorrectLetters(currentLetters => [...currentLetters, letter])
+					} else {
+						// showNotification()
+					}
+				} else {
+					if (!wrongLetters.includes(letter)) {
+						setWrongLetters(currentLetters => [...currentLetters, letter])
+					} else {
+						// showNotification()
+					}
+				}
+			}
+		}
+
+		window.addEventListener('keydown', handleKeydown)
+
+		return () => window.removeEventListener('keydown', handleKeydown)
+	}, [correctLetters, wrongLetters, playable])
+
 	return (
 		<>
 			<Header />
@@ -22,7 +49,7 @@ function App() {
 				<Word selectedWord={selectedWord} correctLetters={correctLetters} />
 			</div>
 		</>
-	);
+	)
 }
 
 export default App;

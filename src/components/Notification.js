b1ecd2463863bbde0e4e65e93c244f41cda37972
Enes Kaplan <eneskaplan94@hotmail.com>
2022-02-24 09:54:19 +0300

    Removed import React statements

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Notification = ({ showNotification }) => {
 	return (
 		<div className={`notification-container ${showNotification ? 'show' : ''}`}>
--- src/components/Word.js
+++ src/components/Word.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Word = ({ selectedWord, correctLetters }) => {
 	return (
 		<div className="word">

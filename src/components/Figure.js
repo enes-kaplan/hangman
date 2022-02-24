8f726b13d8769c381cf711ebcbeeb9961c272e90
Enes Kaplan <eneskaplan94@hotmail.com>
2022-02-24 08:55:45 +0300

    Updating figure on wrongLetters

--- src/components/Figure.js
+++ src/components/Figure.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
-const Figure = () => {
+const Figure = ({ wrongLetters }) => {
+	const errorCount = wrongLetters.length
+
 	return (
 		<svg height="250" width="200" className="figure-container">
 			{/* Rod */}
@@ -10,15 +12,15 @@ const Figure = () => {
 			<line x1="20" y1="230" x2="100" y2="230" />
 
 			{/* Head */}
-			<circle cx="140" cy="70" r="20" className="figure-part" />
+			{errorCount > 0 && <circle cx="140" cy="70" r="20" />}
 			{/* Body */}
-			<line x1="140" y1="90" x2="140" y2="150" className="figure-part" />
+			{errorCount > 1 && <line x1="140" y1="90" x2="140" y2="150" />}
 			{/* Arms */}
-			<line x1="140" y1="120" x2="120" y2="100" className="figure-part" />
-			<line x1="140" y1="120" x2="160" y2="100" className="figure-part" />
+			{errorCount > 2 && <line x1="140" y1="120" x2="120" y2="100" />}
+			{errorCount > 3 && <line x1="140" y1="120" x2="160" y2="100" />}
 			{/* Legs */}
-			<line x1="140" y1="150" x2="120" y2="180" className="figure-part" />
-			<line x1="140" y1="150" x2="160" y2="180" className="figure-part" />
+			{errorCount > 4 && <line x1="140" y1="150" x2="120" y2="180" />}
+			{errorCount > 5 && <line x1="140" y1="150" x2="160" y2="180" />}
 		</svg>
 	)
 }
